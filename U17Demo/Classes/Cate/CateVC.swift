5d9807e5b05f51d050ed2a62b01184d15bed64ab
maowangxin <maowangxin@globalegrow.com>
2021-02-02 18:29:12 +0800

    pod

--- U17Demo/Classes/Cate/CateVC.swift
+++ U17Demo/Classes/Cate/CateVC.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class CateVC: BaseVC {
     
+    lazy var collectionView: UICollectionView = {
+        let lt = UICollectionViewFlowLayout()
+        lt.minimumLineSpacing = 10
+        lt.minimumInteritemSpacing = 10
+        let cw = UICollectionView(frame: CGRect.zero, collectionViewLayout: lt)
+        cw.backgroundColor = UIColor.white
+        cw.delegate = self
+        cw.dataSource = self
+        return cw
+    }()
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    override func initSubView() {
+        view .addSubview(collectionView)
+    }
+    
+    override func layoutSubView() {
+        
+    }
+    
+    
+}
+
+extension CateVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        fatalError()
+    }
+    
+    
+    
 }
