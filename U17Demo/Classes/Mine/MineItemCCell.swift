86876af84a91df83846d35d5d5928286964b4876
maowangxin <maowangxin@globalegrow.com>
2021-04-19 18:50:40 +0800

    adduserCenter

---
+++ U17Demo/Classes/Mine/MineItemCCell.swift
@@ -0,0 +1,63 @@
+//
+//  MineItemCCell.swift
+//  U17Demo
+//
+//  Created by 610582 on 2021/4/19.
+//
+
+import UIKit
+import Kingfisher
+import SnapKit
+
+class MineItemCCell: UBaseCollectionViewCell {
+    
+    var model: MineItemModel? {
+        didSet {
+            guard let model = model else { return }
+            
+            titleLabel.text = model.title
+            
+            let imageName = model.image
+            
+            if imageName.hasPrefix("http") {
+                imageView.kf.setImage(with: URL(string: model.image))
+            } else {
+                imageView.image = UIImage(named: imageName)
+            }
+        }
+    }
+    
+    override func initSubView() {
+        contentView.addSubview(imageView)
+        contentView.addSubview(titleLabel)
+    }
+    
+    override func layoutSubviews() {
+        imageView.snp.makeConstraints {
+            $0.top.equalTo(snp.top)
+            $0.centerX.equalTo(snp.centerX)
+            $0.size.equalTo(CGSize(width: 40, height: 40))
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.bottom).offset(10)
+            $0.centerX.equalTo(snp.centerX)
+        }
+    }
+    
+    lazy var imageView: UIImageView = {
+        let img = UIImageView(frame: .zero)
+        img.contentMode = .scaleAspectFill
+        return img
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        let lab = UILabel(frame: .zero)
+        lab.backgroundColor = .clear
+        lab.textAlignment = .center
+        lab.font = .systemFont(ofSize: 14)
+        lab.textColor = .black
+        return lab
+    }()
+    
+}
---
+++ U17Demo/Classes/Mine/MineItemModel.swift
@@ -0,0 +1,24 @@
+//
+//  MineItemModel.swift
+//  U17Demo
+//
+//  Created by 610582 on 2021/4/19.
+//
+
+import Foundation
+import KakaJSON
+
+struct MineItemModel: Convertible {
+    let id: String = ""
+    var title: String = ""
+    var click_url: String = ""
+    var image: String = ""
+    let description: String = ""
+    let update_time: String = ""
+}
+
+struct MineListModel: Convertible {
+    var title: String = ""
+    var list: [MineItemModel] = []
+    
+}
