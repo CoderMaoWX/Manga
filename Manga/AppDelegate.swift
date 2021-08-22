b4ed130d1cf6f1b58ab5cc02145a0d3b9db94587
maowangxin <maowangxin@globalegrow.com>
2021-08-23 00:13:48 +0800

    test

--- Manga/AppDelegate.swift
+++ Manga/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
         ///开启网络监听
         networkListen()
         
